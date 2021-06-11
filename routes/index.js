40a4da5e71564f301be656e7c0e08c8aa8799220
Paola Guarasci <paolaguarasci@gmail.com>
2021-06-11 16:47:44 +0200

    my-mariadb-galera.paola-galera-3.svc.cluster.local

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require('express')
 var router = express.Router()
 
 const pool = mariadb.createConnection({
-  host: 'database',
+  host: 'my-mariadb-galera.paola-galera-3.svc.cluster.local',
   user: 'paola',
   password: 'root',
   database: 'bg',
